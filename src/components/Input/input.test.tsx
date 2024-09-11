f432bb661f4d7c2e528b3a604f5149b63be113f9
MarcosVel <marcosvelosodev@gmail.com>
2024-09-11 01:22:34 -0300

    test: debug test of input

---
+++ src/components/Input/input.test.tsx
@@ -0,0 +1,9 @@
+import { render } from "@testing-library/react-native";
+import { Input } from ".";
+
+describe("Component: Input", () => {
+  it("should render without activity indicator", () => {
+    const { debug } = render(<Input />);
+    debug();
+  });
+});
