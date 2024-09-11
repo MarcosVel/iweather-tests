4a7a62f99916064404c9d3b39f4bdebf8edbfe34
MarcosVel <marcosvelosodev@gmail.com>
2024-09-11 01:48:48 -0300

    test: query by id without prop

--- src/components/Input/input.test.tsx
+++ src/components/Input/input.test.tsx
@@ -5,7 +5,7 @@ describe("Component: Input", () => {
   it("should render without activity indicator if isLoading prop is undefined", () => {
     render(<Input />);
 
-    const activityIndicator = screen.getByTestId("activity-indicator");
+    const activityIndicator = screen.queryByTestId("activity-indicator");
     expect(activityIndicator).toBeNull();
   });
 });
