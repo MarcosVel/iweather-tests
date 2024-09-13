e2889414e973f3e9da63cf703556448aba33fc14
MarcosVel <marcosvelosodev@gmail.com>
2024-09-13 18:49:00 -0300

    test: should return storage saved city

--- src/libs/asyncStorage/cityStorage.test.ts
+++ src/libs/asyncStorage/cityStorage.test.ts
@@ -1,9 +1,24 @@
-import { getStorageCity } from "./cityStorage";
+import { CityProps } from "@services/getCityByNameService";
+import { getStorageCity, saveStorageCity } from "./cityStorage";
 
 describe("Storage: CityStorage", () => {
   it("should return null when there is no city in storage", async () => {
     const response = await getStorageCity();
 
-    console.log(response);
+    expect(response).toBeNull();
+  });
+
+  it("should return storage saved city", async () => {
+    const newCity: CityProps = {
+      id: "123",
+      name: "Sao Paulo",
+      longitude: 123,
+      latitude: 456,
+    };
+
+    await saveStorageCity(newCity);
+    const response = await getStorageCity();
+
+    expect(response).toEqual(newCity);
   });
 });
