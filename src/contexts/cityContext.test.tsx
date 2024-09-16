6e353874018cd7545593bf77d6b2ec278fe61fd8
MarcosVel <marcosvelosodev@gmail.com>
2024-09-16 09:50:38 -0300

    test: testing useCity hook with CityContext

--- src/contexts/cityContext.test.tsx
+++ src/contexts/cityContext.test.tsx
@@ -6,6 +6,17 @@ describe("Context: CityContext", () => {
   it("should change selected city", async () => {
     const { result } = renderHook(() => useCity(), { wrapper: CityProvider });
 
-    await waitFor(() => act(() => console.log(result.current)));
+    await waitFor(() =>
+      act(() =>
+        result.current.handleChanceCity({
+          id: "1",
+          name: "Sao Paulo",
+          latitude: 123,
+          longitude: 456,
+        })
+      )
+    );
+
+    expect(result.current.city?.name).toBe("Sao Paulo");
   });
 });
