0e74e2235b8862b66da67e65df31e76d3902f97c
MarcosVel <marcosvelosodev@gmail.com>
2024-09-16 10:16:47 -0300

    test: Testing route

--- src/routes/routes.test.tsx
+++ src/routes/routes.test.tsx
@@ -1,9 +1,11 @@
-import { render } from "@testing-library/react-native";
+import { render, screen, waitFor } from "@testing-library/react-native";
 import { Routes } from ".";
 
 describe("Routes", () => {
-  it("should render Search screen when no city is selected", () => {
-    const { debug } = render(<Routes />);
-    debug();
+  it("should render Search screen when no city is selected", async () => {
+    render(<Routes />);
+
+    const title = await waitFor(() => screen.findByText(/escolha um local/i));
+    expect(title).toBeTruthy();
   });
 });
