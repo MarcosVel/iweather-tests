a25474208f6e0773947d3bff22cce07d6d157369
MarcosVel <marcosvelosodev@gmail.com>
2024-09-17 22:43:26 -0300

    test: waiting for loading to finish

--- src/screens/Dashboard/dashboard.test.tsx
+++ src/screens/Dashboard/dashboard.test.tsx
@@ -1,8 +1,14 @@
+import { mockCityAPIResponse } from "@__tests__/mocks/mockCityAPIResponse";
 import { mockWeatherAPIResponse } from "@__tests__/mocks/mockWeatherAPIResponse";
-import { render, screen, waitFor } from "@__tests__/utils/customRender";
+import {
+  render,
+  screen,
+  waitFor,
+  waitForElementToBeRemoved,
+} from "@__tests__/utils/customRender";
+import { saveStorageCity } from "@libs/asyncStorage/cityStorage";
 import { api } from "@services/api";
 import { Dashboard } from ".";
-import { saveStorageCity } from "@libs/asyncStorage/cityStorage";
 
 describe("Screen: Dashboard", () => {
   it("should show city weather", async () => {
@@ -15,11 +21,39 @@ describe("Screen: Dashboard", () => {
       longitude: 456,
     };
 
-    saveStorageCity(city);
+    await saveStorageCity(city);
 
     render(<Dashboard />);
 
     const cityName = await waitFor(() => screen.findByText(/varginha/i));
     expect(cityName).toBeTruthy();
+  });
+
+  it("should show another selected city weather.", async () => {
+    const city = {
+      id: "1",
+      name: "Varginha, MG",
+      latitude: 123,
+      longitude: 456,
+    };
+
+    await saveStorageCity(city);
+
+    /**
+     * Need to:
+     * 1 - fetch weather info of the first selected city
+     * 2 - fetch info of the city
+     * 3 - fetch weather info of the new city selected
+     */
+
+    jest
+      .spyOn(api, "get")
+      .mockResolvedValueOnce({ data: mockWeatherAPIResponse })
+      .mockResolvedValueOnce({ data: mockCityAPIResponse })
+      .mockResolvedValueOnce({ data: mockWeatherAPIResponse });
+
+    render(<Dashboard />);
+
+    await waitForElementToBeRemoved(() => screen.queryByTestId("loading"));
   });
 });
